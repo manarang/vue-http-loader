1a7773c3297583a9033ec131670c5ae81ca96da4
Petrus Jonatan <64063361+manarang@users.noreply.github.com>
2023-05-31 18:38:29 +0700

    Create main.vue

---
+++ hal/main.vue
@@ -0,0 +1,53 @@
+<template><template>
+ <section class="hero-section">
+            <div class="section-overlay "></div>  
+            <div class="container cf  align-items-center"> 
+                <div class="row">   
+                    <div class="col-12  mb-1 text-center">
+                        <h2 class="text-primary">Alumni 1965 </h2>
+                        <h3 class="text-info mb-5">SMPK3&nbsp;&nbsp; Diponegoro</h3>
+                    </div>
+                    <div class="col-lg-12 col-12 mb-5 d-flex flex-column flex-lg-row text-center">
+                        <div class="date-wrap">
+                            <h5 class="text-white">
+                                <i class="custom-icon bi-clock me-2"></i>
+                                1963 - 1965
+                            </h5>
+                        </div>
+                        <div class="location-wrap mx-1 py-3 py-lg-0">
+                            <h5 class="text-white">
+                                <i class="custom-icon bi-geo-alt me-2"></i>
+                                Jl. Diponegoro 78 Jakarta
+                            </h5>
+                        </div>
+                    </div>  
+                </div>  
+            </div>
+ </section>
+</template>
+ <section class="hero-section">
+            <div class="section-overlay "></div>  
+            <div class="container cf  align-items-center"> 
+                <div class="row">   
+                    <div class="col-12  mb-1 text-center">
+                        <h2 class="text-primary">Alumni 1965 </h2>
+                        <h3 class="text-info mb-5">SMPK3&nbsp;&nbsp; Diponegoro</h3>
+                    </div>
+                    <div class="col-lg-12 col-12 mb-5 d-flex flex-column flex-lg-row text-center">
+                        <div class="date-wrap">
+                            <h5 class="text-white">
+                                <i class="custom-icon bi-clock me-2"></i>
+                                1963 - 1965
+                            </h5>
+                        </div>
+                        <div class="location-wrap mx-1 py-3 py-lg-0">
+                            <h5 class="text-white">
+                                <i class="custom-icon bi-geo-alt me-2"></i>
+                                Jl. Diponegoro 78 Jakarta
+                            </h5>
+                        </div>
+                    </div>  
+                </div>  
+            </div>
+ </section>
+</template>
